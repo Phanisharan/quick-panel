e1451ce6b4b0a8976439bf3c7aef42c03fd7e459
raghunadhdasi <raghunadhdasi@gmail.com>
2025-09-03 22:36:31 +0530

    changes have made to all sections

--- js/middle.js
+++ js/middle.js
@@ -1,6 +1,8 @@
 function switchOn(buttonElement) {
     if (buttonElement.querySelector("i")?.textContent === "brightness_4") {
     document.querySelector(".middle").classList.toggle("dark-mode");
+    document.querySelector(".top").classList.toggle("dark-mode");
+    document.querySelector(".bottom").classList.toggle("dark-mode");
     buttonElement.classList.toggle("active");
     return;
   }
